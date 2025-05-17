d6e94b27298d123623d1b02903793a9a2d2ba717
penakelex <117099964+penakelex@users.noreply.github.com>
2025-05-17 14:47:08 +0700

    Update input fields

--- src/presentation/enter/enter_view.rs
+++ src/presentation/enter/enter_view.rs
@@ -10,7 +10,7 @@ use crate::{data::repository::ciphered_file_type::CipheredFileType, enter::compo
 
 #[derive(Clone)]
 pub struct GetRatingData {
-    pub password: u32,
+    pub password: u16,
     pub file_path: String,
     pub file_type: CipheredFileType,
 }
@@ -30,49 +30,49 @@ pub fn EnterView(properties: EnterViewProperties) -> Element {
     let mut is_picking_file = use_signal(|| false);
 
     rsx! {
-        div {
-            display: "flex",
-            flex_direction: "row",
-            gap: "12px",
-            padding_left: "12px",
-            padding_right: "12px",
-            box_sizing: "border-box",
-            width: "100%",
-
             div {
-                flex: "1 1 0",
-                min_width: 0,
-
-                LabeledTextField {
-                    label: t!("password_label"),
-                    placeholder: t!("password_hint"),
-                    text: password,
-                    on_input: move |event: Event<FormData>| {
-                        let new_password = event.value();
-
-                        is_password_correct.set(
-                            new_password.is_ascii()
-                            && new_password.len() <= 5
-                            && new_password.bytes().all(|symbol| symbol.is_ascii_digit())
-                        );
-
-                        password.set(new_password);
-                    },
-                    is_error: !is_password_correct(),
-                    icon: "password",
-                }
-            }
-
-            div {
-                flex: "1 1 0",
-                min_width: 0,
+                display: "flex",
+                flex_direction: "row",
+                gap: "12px",
+                padding_left: "12px",
+                padding_right: "12px",
+                box_sizing: "border-box",
+                width: "100%",
 
                 div {
-                    width: "100%",
-                    flex: "column",
+                    flex: "1 1 0",
+                    min_width: 0,
 
-                    style {
-                        r#"
+                    LabeledTextField {
+                        label: t!("password_label"),
+                        placeholder: t!("password_hint"),
+                        text: password,
+                        on_input: move |event: Event<FormData>| {
+                            let new_password = event.value();
+
+                            is_password_correct.set(
+                                new_password.is_ascii()
+                                && new_password.len() <= 5
+                                && new_password.bytes().all(|symbol| symbol.is_ascii_digit())
+                            );
+
+                            password.set(new_password);
+                        },
+                        is_error: !is_password_correct(),
+                        icon: "password",
+                    }
+                }
+
+                div {
+                    flex: "1 1 0",
+                    min_width: 0,
+
+                    div {
+                        width: "100%",
+                        flex: "column",
+
+                        style {
+                            r#"
                             .button {{
                                 width: 100%;
                                 background: rgb(0, 95, 156);
@@ -83,6 +83,7 @@ pub fn EnterView(properties: EnterViewProperties) -> Element {
                                 font-size: 16px;
                                 font-family: sans-serif;
                                 animation: ripple 0.6s linear;
+                                cursor: pointer;
                             }}
                             .button:hover {{
                                 background: rgb(20, 108, 164);
@@ -92,103 +93,110 @@ pub fn EnterView(properties: EnterViewProperties) -> Element {
                                 box-shadow: 0 1px 2px rgba(0,0,0,0.2);
                             }}
                         "#
-                    }
+                        }
 
-                    LabeledTextField {
-                        label: t!("file_label"),
-                        placeholder: t!("file_hint"),
-                        text: file_path,
-                        on_input: move |event: Event<FormData>| {
-                            file_path.set(event.value());
-                            is_file_valid.set(true);
-                        },
-                        is_error: !is_file_valid(),
-                        icon: "insert_drive_file",
+                        LabeledTextField {
+                            label: t!("file_label"),
+                            placeholder: t!("file_hint"),
+                            text: file_path,
+                            on_input: move |event: Event<FormData>| {
+                                file_path.set(event.value());
+                                is_file_valid.set(true);
+                            },
+                            is_error: !is_file_valid(),
+                            icon: "insert_drive_file",
+                        }
+
+                        button { class: "button",
+                            onclick: move |_| {
+                                if is_picking_file() {
+                                    return;
+                                }
+
+                                is_picking_file.set(true);
+
+                                spawn(async move {
+                                    if let Some(file) = AsyncFileDialog::new()
+                                        .add_filter(t!("file_label"), &["rpf", "zip"])
+                                        .pick_file()
+                                        .await
+                                    {
+                                        if let Some(file_path_from_dialog) = file.path().to_str() {
+                                            file_path.set(file_path_from_dialog.to_string());
+                                            is_file_valid.set(true);
+                                        }
+                                    }
+
+                                    is_picking_file.set(false);
+                                });
+                            },
+
+                            {t!("file_selection_button_label")}
+                        }
                     }
+                }
+
+                div {
+                    flex: "1 1 0",
+                    min_width: 0,
+                    margin_top: "auto",
 
                     button { class: "button",
+                        display: "flex",
+                        align_items: "center",
+                        justify_content: "center",
+                        gap: "4px",
+
                         onclick: move |_| {
-                            if is_picking_file() {
+                            let password = password();
+                            let file_path = file_path();
+
+                            if !password.is_ascii() || !matches!(password.len(), 3..=5)
+                                || password.bytes().any(|symbol| !symbol.is_ascii_digit())
+                            {
+                                is_password_correct.set(false);
+                            }
+
+                            let path = PathBuf::from(file_path.as_str());
+                            let Some(extension) = path.extension() else {
+                                is_file_valid.set(false);
+                                return;
+                            };
+
+                            if !path.exists() || extension != "rpf" && extension != "zip" {
+                                is_file_valid.set(false);
+                            }
+
+                            let Ok(password) = password.parse::<u16>() else {
+                                is_password_correct.set(false);
+                                return;
+                            };
+
+                            if !is_password_correct() || !is_file_valid() {
                                 return;
                             }
 
-                            is_picking_file.set(true);
+                            let Some(file_type) = CipheredFileType::try_from_str(extension.to_str().unwrap_or("")) else {
+                                return;
+                            };
 
-                            spawn(async move {
-                                if let Some(file) = AsyncFileDialog::new()
-                                    .add_filter(t!("file_label"), &["rpf", "zip"])
-                                    .pick_file()
-                                    .await
-                                {
-                                    if let Some(file_path_from_dialog) = file.path().to_str() {
-                                        file_path.set(file_path_from_dialog.to_string());
-                                        is_file_valid.set(true);
-                                    }
-                                }
 
-                                is_picking_file.set(false);
+
+                            properties.on_search.call(GetRatingData {
+                                password,
+                                file_path,
+                                file_type,
                             });
                         },
 
-                        {t!("file_selection_button_label")}
+                        MaterialIcon {
+                            name: "search",
+                            size: 16,
+                        }
+
+                        {t!("search_button_label")}
                     }
-                }
-            }
-
-            div {
-                flex: "1 1 0",
-                min_width: 0,
-                margin_top: "auto",
-
-                button { class: "button",
-                    display: "flex",
-                    align_items: "center",
-                    justify_content: "center",
-                    gap: "4px",
-
-                    onclick: move |_| {
-                        let password = password();
-                        let file_path = file_path();
-
-                        if !password.is_ascii() || !(4..=5).contains(&password.len())
-                            || password.bytes().any(|symbol| !symbol.is_ascii_digit())
-                        {
-                            is_password_correct.set(false);
-                        }
-
-                        let path = PathBuf::from(file_path.as_str());
-                        let Some(extension) = path.extension() else {
-                            is_file_valid.set(false);
-                            return;
-                        };
-
-                        if !path.exists() || extension != "rpf" && extension != "zip" {
-                            is_file_valid.set(false);
-                        }
-
-                        if !is_password_correct() || !is_file_valid() {
-                            return;
-                        }
-
-                        let Some(file_type) = CipheredFileType::try_from_str(extension.to_str().unwrap_or("")) else {
-                            return;
-                        };
-
-                        properties.on_search.call(GetRatingData {
-                            password: password.parse::<u32>().unwrap(),
-                            file_path,
-                            file_type,
-                        });
-                    },
-
-                    MaterialIcon {
-                        name: "search",
-                        size: 16,
-                    }
-
-                    {t!("search_button_label")}
                 }
             }
         }
-    }
 }
